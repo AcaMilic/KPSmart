746e71570db267514545a8699e99df72df60b530
AcaMilic <aleksandarmilic1995@gmail.com>
2024-09-26 22:02:56 +0200

    nesto

--- includes/sections/footer.php
+++ includes/sections/footer.php
@@ -1,38 +1,21 @@
-
-  <!-- Footer -->
-  <footer class="navigation-footer">
+<footer class="navigation-footer">
     <div class="roww navigation-padding">
+      <div class="col-1-of-3 footer-logo">
+      <a class="navbar-brand" href="index.html">
+        <img src="images/logo_dark_bg.png" alt="Logo">
+        <span class="logo-text">KP Smart GRids</span>
+    </a>
+      </div> 
       <div class="col-1-of-3">
-        <div class="navigation-footer-links">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur sequi rerum, at, dignissimos autem suscipit possimus dolores similique cumque omnis culpa magnam expedita est molestias, a maxime! Voluptas velit aut perferendis magni ratione aspernatur inventore at nam facere? Blanditiis ex eligendi commodi, itaque eaque autem beatae eos ab porro aut?</p>
-        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga dicta voluptate omnis quas quisquam. Commodi ab sunt corporis ratione beatae aperiam asperiores, adipisci explicabo dolorem provident! Expedita quos error magnam ipsa vero mollitia fuga maxime autem exercitationem. Ex, mollitia! Doloremque accusamus ab rerum quam ipsa tempore quas cupiditate hic ratione.</p>
       </div>
       <div class="col-1-of-3">
         <div class="phone-and-mail">
-          <span class="green">Tel:</span><span> +38123-456-789</span>
+          <span class="green"><i class="fa-solid fa-phone"></i> </span><span> +38123-456-789</span>
           <br>
-          <span class="green">Mail:</span><span>KPSmartGRids@gmail.com</span>
+          <span class="green"><i class="fa-regular fa-envelope"></i> </span><span>info@smart-grids.rs</span>
           <br>
-          <span class="green">Address: <span> Address 23/45</span>
-        </div>
-      </div>
-      <div class="col-1-of-3">
-        <div class="icons">
-          <a href="#">
-            <i class="fab fa-codepen"></i>
-          </a>
-          <a href="#">
-            <i class="fab fa-linkedin-in"></i>
-          </a>
-          <a href="#">
-            <i class="fab fa-github"></i>
-          </a>
-          <a href="#">
-            <i class="fab fa-facebook-f"></i>
-          </a>
         </div>
       </div>
     </div>
   </footer>
-
-  <!--/ Footer -->
