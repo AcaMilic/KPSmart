ab82f218fbf6353e2fe60b61786141f721688e38
AcaMilic <aleksandarmilic1995@gmail.com>
2024-10-21 15:31:19 +0200

    123

--- form/sendemail.php
+++ form/sendemail.php
@@ -5,13 +5,15 @@ if (isset($_POST['name']) && isset($_POST['message']) && isset($_POST['email']))
     $mailFrom = $_POST['email'];
 
     $mailTo = "aleksandarmilic1995@gmail.com";
-    $headers = "From: ".$mailFrom."\r\n" ."Reply-To: ".$mailFrom."\r\n";
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    $headers = "From: ".$mailFrom."\r\n" ."Message: ".$message."\r\n";
+    $txt = "You have received an e-mail from ".$name.".\n\n";
 
-    if (mail($mailTo, "Contact Form Submission", $txt, $headers)) {
-        echo "Email sent successfully";
+    if (mail($mailTo, $txt, $headers)) {
+        echo json_encode(["status" => "success", "message" => "Message sent successfully!"]);
     } else {
-        echo "Email sending failed";
+        echo json_encode(["status" => "error", "message" => "Message could not be sent."]);
     }
+} else {
+    echo json_encode(["status" => "error", "message" => "Invalid input."]);
 }
 ?>
